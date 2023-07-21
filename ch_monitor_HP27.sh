6332e49d5de6a8941a560dfeea36efdb321d540e
kahunsyo <contact2ichino@gmail.com>
2023-07-21 09:00:23 +0900

    init

---
+++ ch_monitor_HP27.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# single monitor with HP27
+xrandr --output DP-0 --mode 2560x1440 --rate 165 --output HDMI-0 --off
---
+++ ch_monitor_IODATA32.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# single monitor with HP27
+xrandr --output HDMI-0 --mode 2560x1440 --rate 60 --output DP-0 --off
